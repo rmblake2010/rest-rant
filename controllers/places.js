1b1b474320492a8a37f644c061f43f9ba0956872
Ryan Blake <xbungalowx@gmail.com>
2022-02-26 08:04:47 -0600

    added places view & mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [
+        {
+            name: '76 diner',
+            city: 'ottawa',
+            state: 'Kansas',
+            cuisines: 'diner',
+            pic: 'src = '
+        },
+        {
+            name: 'prime time grill',
+            city: 'ottawa',
+            state: 'Kansas',
+            cuisines: 'pub & grill',
+            pic: 'src = '
+        }
+    ]
+    res.render('places/index', {places})
 })
 
 module.exports = router
