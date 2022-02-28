a9419bb72a4f1bb2c64f2edd4866a392c3b9eff8
Ryan Blake <xbungalowx@gmail.com>
2022-02-27 20:42:27 -0600

    adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,9 +5,14 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST Rant!</h1>
+              <div  className='d-flex justify-content-around'>
+                    <img src='/images/burger.jpg' alt='burger with knife in it'/>
+                    <img src='/images/homePage.jpg' alt='kebabs on wooden plate' />
+                    <img src="/images/sausage.jpg" alt="sausages and fruit" />
+              </div>
               <a href='/places'>
-                    <button className='btn-primary d-block mx-auto'>Places Page</button>
+                    <button className='btn-primary d-block mx-auto mt-2'>Come see our places!</button>
               </a>
           </main>
       </Def>
---
+++ views/places/new.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Def = require('../default')
+
+
+function new_form () {
+    return(
+        <Def>
+            <h1>Add a New Place!</h1>
+        </Def>
+    )
+}
+
+module.exports = new_form
