a075e14db41a62b091d3ead4aa19746ab9dfdfff
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 07:39:58 -0500

    added atlas db

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, () => {
+    console.log("listening on port: ", process.env.PORT)
+})
--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
-})
+}, () => {console.log('connect to db: ', process.env.MONGO_URI)})
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comments')
