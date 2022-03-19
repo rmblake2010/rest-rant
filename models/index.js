8e65be56e38b26852618603e7598d0d38be1fbcc
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 11:16:46 -0500

    revert

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
-  if(err) throw err;
-})
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}, () => {console.log('connect to db: ', process.env.MONGO_URI)})
+
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comments')
